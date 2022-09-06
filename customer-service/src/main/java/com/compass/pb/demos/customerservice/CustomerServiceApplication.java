fc79d39d2035e689f457a9e22ea3b20404a9e345
Reginaldo C Domingos <reginaldo.domingos@compasso.com.br>
2022-09-06 16:30:54 -0300

    added cors liberation

--- customer-service/src/main/java/com/compass/pb/demos/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/compass/pb/demos/customerservice/CustomerServiceApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -14,6 +17,17 @@ public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/customer/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
--- pet-service/src/main/java/com/compass/pb/demos/petservice/PetServiceApplication.java
+++ pet-service/src/main/java/com/compass/pb/demos/petservice/PetServiceApplication.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -12,6 +15,17 @@ public class PetServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/pet/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
